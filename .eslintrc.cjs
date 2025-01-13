788ccbba06bf7a57cecbca315e48812df7e79e99
namkyung.lee <iddbxk@daum.net>
2025-01-13 16:58:00 +0900

    chore remove unnecessary eslint configurations

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}; 
